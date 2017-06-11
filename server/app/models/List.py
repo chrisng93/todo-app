fe9667835ee46d75fae3ba393a7c62f42e55032d
Chris Ng <chrisng@Chriss-MBP.attlocal.net>
2017-06-10 18:01:03 -0700

    add todos to list to_json method

--- server/app/models/List.py
+++ server/app/models/List.py
@@ -8,10 +8,12 @@ class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow())
     name = db.Column(db.String(50), nullable=False)
-    todos = db.relationship('Todo', backref='list', lazy='dynamic')
+    todos = db.relationship('Todo', backref='list', lazy='joined')
 
     def to_json(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        json = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        json['todos'] = [todo.to_json() for todo in self.todos]
+        return json
 
     def from_json(self, json):
         if 'name' in json:
